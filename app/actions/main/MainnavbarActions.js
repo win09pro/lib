fd46b7a07127a453f0ee09b9751bcb4bc919d37c
51202574 <51202574@hcmut.edu.vn>
2016-04-23 17:26:18 +0700

    123

---
+++ app/actions/main/MainnavbarActions.js
@@ -0,0 +1,57 @@
+import alt from '../../alt';
+class MainnavbarActions {
+  constructor() {
+    this.generateActions(
+      'getIntroCateSuccess',
+      'GetIntroCateFail',
+      'getServiceCateSuccess',
+      'GetServiceCateFail',
+      'getHelpCateSuccess',
+      'getHelpCateFail'
+    );
+  }
+  getIntroduceCategory()
+  {
+    $.ajax({
+      type:'POST',
+      url:'/api/getCategory',
+      data:{type:1}
+    })
+    .done((data) => {
+      this.actions.getIntroCateSuccess(data);
+    })
+    .fail((jqXhr) =>{
+      this.actions.GetIntroCateFail(jqXhr.responseJSON.message);
+    });
+  }
+  getServiceCategory()
+  {
+     $.ajax({
+      type:'POST',
+      url:'/api/getCategory',
+      data:{type:2}
+    })
+    .done((data) => {
+      this.actions.getServiceCateSuccess(data);
+    })
+    .fail((jqXhr) =>{
+      this.actions.GetServiceCateFail(jqXhr.responseJSON.message);
+    });
+  }
+   getHelpCategory()
+  {
+     $.ajax({
+      type:'POST',
+      url:'/api/getCategory',
+      data:{type:3}
+    })
+    .done((data) => {
+      this.actions.getHelpCateSuccess(data);
+    })
+    .fail((jqXhr) =>{
+      this.actions.getHelpCateFail(jqXhr.responseJSON.message);
+    });
+  }
+}
+
+export default alt.createActions(MainnavbarActions);
